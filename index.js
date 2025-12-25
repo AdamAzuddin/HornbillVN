71a60c5135cbb880eb1dc31aaba0c4cc479109e7
Adam Azuddin <05adamaz@gmail.com>
2025-12-25 08:42:59 +0800

    added smooth scroll when moving between passages

---
+++ index.js
@@ -0,0 +1,6 @@
+window.scrollTo(0, 0);
+
+// Optional: Force the browser to treat the transition as a fresh start
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
